42a0d3026373eefdcd82981014d5f424cbebbfc3
Diyath Sahan Rajapakshe <diyathrajapakshe@gmail.com>
2023-12-20 02:42:42 +0530

    Bethel-zkEVM-contract-verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
 // hardhat.config.js
 require("@nomiclabs/hardhat-ethers");
 require("@nomicfoundation/hardhat-verify");
-
+require("@ericxstone/hardhat-blockscout-verify");
+const { SOLIDITY_VERSION, EVM_VERSION } = require("@ericxstone/hardhat-blockscout-verify");
 
 require('dotenv').config()
 
@@ -15,15 +16,37 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.23",
   networks: {
-    hardhat: {},
-    mumbai: {
-      url: process.env.ALCHEMY_API_URL,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    BethelzkEVM: {
+      url: 'https://rpc-testnet-zkevm.bethel.network',
+      accounts: env.PRIVATE_KEY,
+    }
+  },
+    blockscoutVerify: {
+      blockscoutURL: "https://testnet-zkevm.bethel.network",
+      contracts: {
+        "BETHEL": {
+          compilerVersion: SOLIDITY_VERSION.env.SOLIDITY_VERSION, // checkout enum SOLIDITY_VERSION
+          optimization: false,
+          evmVersion: EVM_VERSION.env.EVM_VERSION, // checkout enum EVM_VERSION
+          optimizationRuns: 999999,
+        },
+      },
     },
-  },
   etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY
-  },
+    apiKey: {
+      BethelzkEVM: env.ETHERSCAN_API_KEY
+    },
+    customChains: [
+      {
+        network: "BethelzkEVM",
+        chainId: 1122,
+        urls: {
+          apiURL: "https://testnet-zkevm.bethel.network/api",
+          browserURL: "https://testnet-zkevm.bethel.network"
+        }
+      }
+    ]
+  }
 };
